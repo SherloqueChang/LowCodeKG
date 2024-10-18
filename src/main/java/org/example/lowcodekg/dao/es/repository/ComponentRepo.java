fb93d6c1301cef569982b0626e35bf4d939808f8
SherloqueChang <1410945936@qq.com>
2024-10-18 19:20:27 +0800

    新增Component实体类和对应的Elasticsearch仓库接口
    - 在org.example.lowcodekg.dao.es.document包下添加了Component类，作为Elasticsearch文档映射类
    - 在org.example.lowcodekg.dao.es.repository包下添加了ComponentRepo接口，继承自ElasticsearchRepository，用于操作Component实体

---
+++ src/main/java/org/example/lowcodekg/dao/es/repository/ComponentRepo.java
@@ -0,0 +1,8 @@
+package org.example.lowcodekg.dao.es.repository;
+
+import org.example.lowcodekg.dao.es.document.Component;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ComponentRepo extends ElasticsearchRepository<Component, Long> {
+
+}

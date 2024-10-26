459a3da07da9de39cb5d44e735e71ea5e4deac0a
unknown <3293215638@qq.com>
2024-10-26 23:52:03 +0800

    Basically Completed. See the CLASS comments for more info.

--- src/main/java/org/example/lowcodekg/extraction/markdown/AntMDExtractor.java
+++ src/main/java/org/example/lowcodekg/extraction/markdown/AntMDExtractor.java
@@ -18,6 +18,24 @@ import lombok.Setter;
 
 /**
  * Markdown文件解析 AntDesign专用
+ * @param DataDir 以AntDesign的github仓库为输入, 需将@DataDir设置为[Folder:ant-design-master]的路径.
+ * 使用 public void extraction() 对指定路径的ant仓库进行解析.
+ * 抽取结果将生成一组 "RawData" 的列表; 每个 "RawData" 对象对应一个组件, 里面包含了文档中给出的对应信息.
+ * 
+ * "RawData" 中包含一个代码示例  "CodeDemo" 列表, 提供了若干代码应用实例.
+ * "RawData" 中包含一个配置项    "RawConfigItem" 列表, 提供了该组件的配置项相关信息.
+ * "RawData"        ---   public void toComponent(Component component)
+ * "RawConfigItem"  ---   public void toConfigItem(ConfigItem config)
+ * 使用上述函数将原始数据转换为schema中的对象.
+ * 
+ * @BugsOrUnfinished
+ * 1. 文档中还有部分信息, 目前与Schema无关, 暂未做任何提取处理.
+ * 2. 目前对于不同组件的.md文件中是否存在可能导致bug的差异, 正在人工检查中.
+ *      # 已知bug: 组件参数部分，部分组件存在疑似"分层级的参数(?)", 当前版本全部没算进来.
+ *      # 已知bug: 大多组件的config信息包含 "版本" 值, 但个别不包括, 目前会导致报错并跳过相关内容.
+ *      # 已知bug: 个别组件在.md文档中给出的代码示例并没有对应的demo.md说明, 会导致IOException.
+ * 3. 配置项信息中存在一些较复杂的内容形式, 目前统一以字符串形式保存.
+ * 
  */
 public class AntMDExtractor extends KnowledgeExtractor {
     // extract results
@@ -25,28 +43,36 @@ public class AntMDExtractor extends KnowledgeExtractor {
 
     // private variables for extracting
     private String WorkDir;     // temp sub-directory for current component
-    private final String[] componentNames = { "button", "affix", "alert" };
-    private int lineNum;
+    private int lineNum;        // temp couter for current line number
     
     // Test function
     public static void main(String[] args) {
-        System.out.println("Hello world: Test AntMDExtractor.");
+        System.out.println("Testing AntMDExtractor.");
         AntMDExtractor test = new AntMDExtractor();
         test.setDataDir("E:\\test\\ant-design-master\\components");
         test.extraction();
         for (int i = 0; i < test.dataList.size(); i++) {
-            test.dataList.get(i).Test();
+            // Debug: output extracted info
+            // test.dataList.get(i).Test();
         }
     }
 
     @Override
     public void extraction() {
-        for (int i = 0; i < componentNames.length; i++) {
-            RawData data = new RawData();
-            WorkDir = this.getDataDir() + "/" + componentNames[i];
-            parseComponent(WorkDir + "/index.zh-CN.md", data);
-            dataList.add(data);
+        File[] folders = new File(this.getDataDir()).listFiles(File::isDirectory);
+        for (int i = 0; i < folders.length; i++) {
+            if (!Files.exists(Paths.get(this.getDataDir(), folders[i].getName(), "index.zh-CN.md"))) {
+                System.err.println(".md File Not Found (May not component): " + folders[i].getName());
+                continue;
+            }
+            else {
+                RawData data = new RawData();
+                WorkDir = this.getDataDir() + "/" + folders[i].getName();
+                parseComponent(WorkDir + "/index.zh-CN.md", data);
+                dataList.add(data);
+            }
         }
+
         return;
     }
 
@@ -65,7 +91,6 @@ public class AntMDExtractor extends KnowledgeExtractor {
     
     private void parseLine(List<String> lines, RawData data) {
         String line = lines.get(lineNum++);
-        if (line.equals("---")) return;
         if (line.startsWith("category: ")) {
             // category / default = Components
             data.setCategory(line.substring(10));
@@ -142,8 +167,40 @@ public class AntMDExtractor extends KnowledgeExtractor {
     }
 
     private void parseConfig(List<String> lines, RawData data) {
-        // TODO: parseAPI info from .md file
+        String line;
+        while (lineNum < lines.size()) {
+            line = lines.get(lineNum);
+            if (line.startsWith("##")) {
+                parseLine(lines, data);
+                return;
+            }
+            if (line.startsWith("| ") && !line.startsWith("| 属性") && !line.startsWith("| 参数") && !line.startsWith("| --- |")) {
+                line = line.substring(1, line.length() - 2);
+                String[] values = line.split(" \\|");
+                if (values.length != 5) {
+                    // Debug:
+                    // System.err.println("Bug or Invalid source data: ConfigItem Values = " + values.length);
+                    // System.err.println("WorkDir = " + WorkDir);
+                    // System.err.println(line);
+                    // for (int i = 0; i < values.length; i++) {
+                    //     System.err.println(values[i]);
+                    // }
+                }
+                else {
+                    RawConfigItem config = new RawConfigItem();
+                    config.setName(values[0].substring(1));
+                    config.setDescription(values[1].substring(1));
+                    config.setType(values[2].substring(1));
+                    config.setDefaultValue(values[3].substring(1));
+                    config.setVersion(values[4].substring(1));
+                    data.getConfigItems().add(config);
+                }
+                
+            }
+            lineNum++;
+        }
     }
+
 }
 
 class RawData {
@@ -174,9 +231,11 @@ class RawData {
     @Getter
     private ArrayList<CodeDemo> codeDemos = new ArrayList<CodeDemo>();
 
+    @Getter
+    private ArrayList<RawConfigItem> configItems = new ArrayList<RawConfigItem>();
+
     // Test function for RawData
     public void Test() {
-        System.out.println("Hello World!");
         System.out.println("name = " + name);
         System.out.println("name_CN = " + name_CN);
         System.out.println("category = " + category);
@@ -185,6 +244,9 @@ class RawData {
         System.out.println("usage = " + usage);
         for (int i = 0; i < codeDemos.size(); i++) {
             codeDemos.get(i).Test();
+        }
+        for (int i = 0; i < configItems.size(); i++) {
+            configItems.get(i).Test();
         }
     }
 
@@ -196,6 +258,7 @@ class RawData {
         component.setDescription(description);
         return;
     }
+
 }
 
 class CodeDemo {
@@ -250,7 +313,43 @@ class CodeDemo {
         System.out.println("description = " + description);
     }
 
-    public void toConfigItem() {
-        // TODO: add function
+}
+
+class RawConfigItem {
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private String type;
+
+    @Getter
+    @Setter
+    private String defaultValue;
+
+    @Getter
+    @Setter
+    private String version;
+
+    // Test function for RawConfigItem
+    public void Test() {
+        System.out.println("name = " + name);
+        System.out.println("description = " + description);
+        System.out.println("type = " + type);
+        System.out.println("defaultValue = " + defaultValue);
+        System.out.println("version = " + version);
+    }
+
+    public void toConfigItem(ConfigItem configItem) {
+        configItem.setCode(name);
+        configItem.setDefaultValue(defaultValue);
+        configItem.setDescription(description);
+        configItem.setType(type);
+        return;
     }
 }

17e6c4c0424b4dccfcdf9cdedb75badadd96dfa1
SherloqueChang <1410945936@qq.com>
2024-11-22 15:11:30 +0800

    feat(schema): 添加 Script 实体类并创建 JavaExtractor 类
    - 在 workflow 包中添加 Script 实体类，包含 name 和 description 字段
    - 新增 JavaExtractor 类，用于 Java 项目的知识提取

---
+++ src/main/java/org/example/lowcodekg/extraction/java/JavaExtractor.java
@@ -0,0 +1,14 @@
+package org.example.lowcodekg.extraction.java;
+
+import org.example.lowcodekg.extraction.KnowledgeExtractor;
+
+/**
+ * Java 项目解析类
+ */
+public class JavaExtractor extends KnowledgeExtractor {
+
+    @Override
+    public void extraction() {
+
+    }
+}
--- src/main/java/org/example/lowcodekg/schema/entity/workflow/Script.java
+++ src/main/java/org/example/lowcodekg/schema/entity/workflow/Script.java
@@ -1,4 +1,17 @@
 package org.example.lowcodekg.schema.entity.workflow;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class Script {
+
+     private String name;
+
+     private String description;
+
+
 }

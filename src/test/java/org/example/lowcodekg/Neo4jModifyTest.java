ce44738db2fa0c0ef33dbaf3ad7a740fd54ff714
SherloqueChang <1410945936@qq.com>
2024-12-27 13:12:10 +0800

    test: 添加创建测试节点的方法- 在 Neo4jModifyTest 类中添加了 createTestNode 方法- 创建并保存了一个 JavaMethodEntity 对象 - 该方法用于测试在 Neo4j 数据库中创建新的方法节点

--- src/test/java/org/example/lowcodekg/Neo4jModifyTest.java
+++ src/test/java/org/example/lowcodekg/Neo4jModifyTest.java
@@ -1,7 +1,9 @@
 package org.example.lowcodekg;
 
 import org.example.lowcodekg.dao.neo4j.entity.JavaClassEntity;
+import org.example.lowcodekg.dao.neo4j.entity.JavaMethodEntity;
 import org.example.lowcodekg.dao.neo4j.repository.JavaClassRepo;
+import org.example.lowcodekg.dao.neo4j.repository.JavaMethodRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,8 @@ public class Neo4jModifyTest {
 
     @Autowired
     private JavaClassRepo javaClassRepo;
+    @Autowired
+    private JavaMethodRepo javaMethodRepo;
 
     @Test
     public void testModify() {
@@ -24,5 +28,18 @@ public class Neo4jModifyTest {
             classEntity.setDescription("modified description");
         }
         javaClassRepo.save(classEntity);
+    }
+
+    @Test
+    public void createTestNode() {
+        JavaMethodEntity methodEntity = new JavaMethodEntity();
+        methodEntity.setName("setCreateTime");
+        methodEntity.setFullName("com.example.aurora-springboot.service.impl.MomentService.setCreateTime");
+        methodEntity.setProjectName("aurora-springboot");
+        methodEntity.setDescription("设置当前动态记录的创建时间为动态发布时间");
+        methodEntity.setContent("public void setCreateTime(Date date) {\n" +
+                "momentRepository.setCreateTime(date);\n" +
+                "}");
+        javaMethodRepo.save(methodEntity);
     }
 }

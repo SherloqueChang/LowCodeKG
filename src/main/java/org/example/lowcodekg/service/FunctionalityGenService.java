db423696515e94f704754ffdc2e0b4bc2379f940
SherloqueChang <1410945936@qq.com>
2025-01-11 20:06:44 +0800

    feat(service): 添加页面功能描述生成方法
    - 在 FunctionalityGenService 接口中添加了 generatePageFunctionality 方法
    - 在 FunctionalityGenServiceImpl 类中实现了 generatePageFunctionality 方法
    - 新增方法用于生成页面实体的功能描述信息

--- src/main/java/org/example/lowcodekg/service/FunctionalityGenService.java
+++ src/main/java/org/example/lowcodekg/service/FunctionalityGenService.java
@@ -1,7 +1,12 @@
 package org.example.lowcodekg.service;
 
+import org.example.lowcodekg.dao.neo4j.entity.page.PageEntity;
+
 /**
  * 实体功能描述信息生成服务
  */
 public interface FunctionalityGenService {
+
+    String generatePageFunctionality(PageEntity pageEntity);
+
 }
--- src/main/java/org/example/lowcodekg/service/impl/FunctionalityGenServiceImpl.java
+++ src/main/java/org/example/lowcodekg/service/impl/FunctionalityGenServiceImpl.java
@@ -1,8 +1,16 @@
 package org.example.lowcodekg.service.impl;
 
+import org.example.lowcodekg.dao.neo4j.entity.page.PageEntity;
 import org.example.lowcodekg.service.FunctionalityGenService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FunctionalityGenServiceImpl implements FunctionalityGenService {
+
+    @Override
+    public String generatePageFunctionality(PageEntity pageEntity) {
+
+        return null;
+    }
+
 }

5093d3cda27ea9b01a234385fedf22526ac52247
SherloqueChang <1410945936@qq.com>
2025-01-11 17:50:18 +0800

    refactor: 添加 WorkflowRepo 的 Repository 注解
    - 在 WorkflowRepo接口上添加了 @Repository 注解
    - 这个改动可以确保 Spring 正确识别该接口为一个数据访问层的仓库

--- src/main/java/org/example/lowcodekg/dao/neo4j/repository/WorkflowRepo.java
+++ src/main/java/org/example/lowcodekg/dao/neo4j/repository/WorkflowRepo.java
@@ -3,7 +3,9 @@ package org.example.lowcodekg.dao.neo4j.repository;
 import org.example.lowcodekg.dao.neo4j.entity.WorkflowEntity;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkflowRepo extends Neo4jRepository<WorkflowEntity, Long> {
 
     @Query("MATCH (s:Workflow) WHERE id(s)=$sid " +

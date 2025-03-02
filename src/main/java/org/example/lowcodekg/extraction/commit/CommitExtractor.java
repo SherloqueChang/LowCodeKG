f933db61cd77b568db0c919567ac02ebc6044fba
SherloqueChang <1410945936@qq.com>
2025-03-02 16:39:35 +0800

    feat(extraction): 添加代码提交分析功能
    - 在 pom.xml 中添加 JGit 依赖，用于处理 Git 相关操作
    - 新增 CommitExtractor 类，用于提取代码提交信息
    - 添加 DiffTest 测试类，演示如何使用 JGit 获取代码差异

---
+++ src/main/java/org/example/lowcodekg/extraction/commit/CommitExtractor.java
@@ -0,0 +1,12 @@
+package org.example.lowcodekg.extraction.commit;
+
+import org.example.lowcodekg.extraction.KnowledgeExtractor;
+
+
+public class CommitExtractor extends KnowledgeExtractor {
+
+    @Override
+    public void extraction() {
+
+    }
+}

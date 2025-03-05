ccfe02a3c6f2aeecc82db7b2acb64576ee886c62
SherloqueChang <1410945936@qq.com>
2025-03-05 09:22:17 +0800

    refactor(schema): 重命名 DeviceLogic 类为 ControlLogic
    -将 DeviceLogic.java 文件重命名为 ControlLogic.java
    - 更新类名以更好地反映其功能和用途

--- src/main/java/org/example/lowcodekg/schema/entity/dsl/ControlLogic.java
+++ src/main/java/org/example/lowcodekg/schema/entity/dsl/ControlLogic.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DeviceLogic {
+public class ControlLogic {
     @JsonProperty("projectUuid")
     private String projectUuid;
 

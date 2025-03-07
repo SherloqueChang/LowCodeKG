f66a37ad2497b924616c91d6eaa473378b0285d2
SherloqueChang <1410945936@qq.com>
2025-03-07 16:25:16 +0800

    feat(extraction): 新增前端页面与后端工作流实体关联类
    - 添加 PageWorkflowLink 类，用于关联前端页面和后端工作

---
+++ src/main/java/org/example/lowcodekg/extraction/fusion/PageWorkflowLink.java
@@ -0,0 +1,16 @@
+package org.example.lowcodekg.extraction.fusion;
+
+import org.example.lowcodekg.extraction.KnowledgeExtractor;
+
+/**
+ * @Description 关联前端页面与后端工作流实体
+ * @Author Sherloque
+ * @Date 2025/3/7 16:13
+ */
+public class PageWorkflowLink extends KnowledgeExtractor {
+
+    @Override
+    public void extraction() {
+
+    }
+}

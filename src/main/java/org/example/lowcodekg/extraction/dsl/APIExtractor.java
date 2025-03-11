20476b6713a67776da84a7853496a7d2f636a416
SherloqueChang <1410945936@qq.com>
2025-03-11 21:19:23 +0800

    feat(extraction): 添加 APIExtractor 类解析 DSL 数据
    - 新增 APIExtractor 类继承 KnowledgeExtractor
    - 实现了 API 数据解析的空方法 extraction

---
+++ src/main/java/org/example/lowcodekg/extraction/dsl/APIExtractor.java
@@ -0,0 +1,16 @@
+package org.example.lowcodekg.extraction.dsl;
+
+import org.example.lowcodekg.extraction.KnowledgeExtractor;
+
+/**
+ * @Description 解析低代码平台DSL的API数据
+ * @Author Sherloque
+ * @Date 2025/3/11 21:18
+ */
+public class APIExtractor extends KnowledgeExtractor {
+
+    @Override
+    public void extraction() {
+
+    }
+}

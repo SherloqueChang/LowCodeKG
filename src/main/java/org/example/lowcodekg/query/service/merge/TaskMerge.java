4587f4c0c3ed5f636ba22de3fd88457dbafbac7c
SherloqueChang <1410945936@qq.com>
2025-03-22 21:02:36 +0800

    feat(query): 添加任务合并接口及实现类
    - 新增 TaskMerge 接口，用于定义任务合并和兼容性检查的方法
    - 实现 TaskMergeImpl 类，提供任务合并的具体实现

---
+++ src/main/java/org/example/lowcodekg/query/service/merge/TaskMerge.java
@@ -0,0 +1,9 @@
+package org.example.lowcodekg.query.service.merge;
+
+/**
+ * @Description 将子任务合并，并进行兼容性检查
+ * @Author Sherloque
+ * @Date 2025/3/22 21:01
+ */
+public interface TaskMerge {
+}
---
+++ src/main/java/org/example/lowcodekg/query/service/merge/TaskMergeImpl.java
@@ -0,0 +1,11 @@
+package org.example.lowcodekg.query.service.merge;
+
+/**
+ * @Description
+ * @Author Sherloque
+ * @Date 2025/3/22 21:02
+ */
+public class TaskMergeImpl implements TaskMerge {
+
+
+}

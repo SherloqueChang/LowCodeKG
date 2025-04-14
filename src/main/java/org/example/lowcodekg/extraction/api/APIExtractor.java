38e824edc18eecaeff72a519764241ce470501da
SherloqueChang <1410945936@qq.com>
2025-04-14 15:47:58 +0800

    feat(extraction): 添加 Mashup API 信息抽取类
    - 新增 APIExtractor 类，用于抽取 Mashup API 信息
    - APIExtractor继承自 KnowledgeExtractor
    -目前 extraction 方法为空，需后续具体实现

---
+++ src/main/java/org/example/lowcodekg/extraction/api/APIExtractor.java
@@ -0,0 +1,14 @@
+package org.example.lowcodekg.extraction.api;
+
+import org.example.lowcodekg.extraction.KnowledgeExtractor;
+
+/**
+ * 抽取 Mashup API 信息
+ */
+public class APIExtractor extends KnowledgeExtractor {
+
+    @Override
+    public void extraction() {
+
+    }
+}
